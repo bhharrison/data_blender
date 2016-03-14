838d330ff2dd6cc1259b5c4f98d0d36f180b71e7
Prelang Builder <builder@prelang.com>
2016-03-14 01:02:11 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'QuoteRequest' to allow for create, update

--- app/admin/quote_request.rb
+++ app/admin/quote_request.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register QuoteRequest do
+  permit_params :website, :callback, :description, :hours, :cost, :nte
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
